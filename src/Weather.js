7659df5dba96658e586ee48d22d0507e39d5c9c3
annaknish <108184361+annaknish@users.noreply.github.com>
2023-03-19 20:26:23 +0200

    Testing 1

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,9 @@
 import React from "react";
-import axios from "axios";
 
 export default function Weather() {
   return (
     <div>
-      <h1>Hi there</h1>
+      <h1>Hi checky</h1>
     </div>
   );
 }
