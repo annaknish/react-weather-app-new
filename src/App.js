81eb4b2bf4b224c1afd3647827d20ca08798e399
annaknish <108184361+annaknish@users.noreply.github.com>
2023-03-26 21:26:05 +0300

    Added HTML structure

--- src/App.js
+++ src/App.js
@@ -6,8 +6,18 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <p>Hello world!!!</p>
           <Weather />
+          <footer>
+            This project was coded by Anna Knysh and is{" "}
+            <a
+              href="https://github.com/annaknish/react-weather-app-new"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and hosted on Netlify
+          </footer>
         </header>
       </div>
     </div>
